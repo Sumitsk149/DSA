24d808dee05cc9b2465905b49d7290cb96fb1f67
Sumit Khandagale <ssk149@gmail.com>
2025-11-12 07:51:49 +0530

    Time: 3 ms (57.88%), Space: 42.1 MB (56.67%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,28 +1,20 @@
-public class Solution
-{
-    public string ReverseWords(string s)
-    {
+public class Solution {
+    public string ReverseWords(string s) {
         StringBuilder sb = new();
-        int end = -1;
 
-        for (int i = s.Length - 1; i >= 0; i--)
+        var sArray = s.Split(" ");
+
+        for(int i = sArray.Length - 1; i >= 0; i--)
         {
-            if (!char.IsWhiteSpace(s[i]))
+            if (!string.IsNullOrEmpty(sArray[i]))
             {
-                if (end == -1)
+                if(sb.Length > 0)
                 {
-                    end = i;
+                    sb.Append($" {sArray[i]}");
                 }
-
-                // If we're at the start of the string or previous char is whitespace, we found a word boundary
-                if (i == 0 || char.IsWhiteSpace(s[i - 1]))
+                else
                 {
-                    if (sb.Length > 0)
-                    {
-                        sb.Append(' ');
-                    }
-                    sb.Append(s.Substring(i, end - i + 1));
-                    end = -1;
+                    sb.Append(sArray[i]);
                 }
             }
         }
