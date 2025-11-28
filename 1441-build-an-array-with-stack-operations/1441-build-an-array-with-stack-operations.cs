b90b50311ee8a15f65a8d1cfb9abb04dac3b7017
Sumit Khandagale <ssk149@gmail.com>
2025-11-28 10:07:42 +0530

    Time: 0 ms (100%), Space: 49.6 MB (92.41%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cs
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cs
@@ -1,38 +1,23 @@
 public class Solution {
     public IList<string> BuildArray(int[] target, int n) 
     {
-        var stack = new Stack<int>();
         var result = new List<string>();
-        var curr = 0;
+        var index = 0;
 
         for(int i = 1; i <= n; i++)
         {
-            if(target.Length == stack.Count && stack.Peek() == target[target.Length - 1])
+            if(target[index] == i)
             {
-                return result;
+                result.Add("Push");
+                index++;
+                if(target.Length == index)
+                    return result;
             }
-
-            if(stack.Count > 0 )
+            else
             {
-                if(curr > 0 && stack.Peek() != target[curr - 1])
-                {
-                    stack.Pop();
-                    result.Add("Pop");
-                }
-                else if (curr == 0 && stack.Peek() != target[curr])
-                {
-                    stack.Pop();
-                    result.Add("Pop");
-                }
+                result.Add("Push");
+                result.Add("Pop");
             }
-
-            stack.Push(i);
-            result.Add("Push");
-            
-            if(i == target[curr])
-            {
-                curr++;
-            }  
         }
 
         return result;
