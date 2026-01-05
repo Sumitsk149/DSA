51444aa09dd7b56a8e62e4769372b6d4b97453a0
Sumit Khandagale <ssk149@gmail.com>
2026-01-05 10:10:01 +0530

    Time: 0 ms (100%), Space: 50.1 MB (38.44%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cs
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cs
@@ -2,25 +2,25 @@ public class Solution {
     public IList<string> BuildArray(int[] target, int n) 
     {
         var result = new List<string>();
-        var index = 0;
+        var count = 0;
 
         for(int i = 1; i <= n; i++)
         {
-            if(target[index] == i)
+            result.Add("Push");
+            count++;
+
+            if(target[count - 1] != i)
             {
-                result.Add("Push");
-                index++;
-                if(target.Length == index)
-                    return result;
-            }
-            else
-            {
-                result.Add("Push");
                 result.Add("Pop");
+                count--;
             }
+
+            if(target.Length == count && target[target.Length - 1] == i)
+            {
+                return result;
+            }            
         }
 
         return result;
-
     }
 }
