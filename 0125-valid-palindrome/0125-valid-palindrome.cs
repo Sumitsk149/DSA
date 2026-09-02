0455984301ebb76156da77d2b6761c783dc1044e
Sumit Khandagale <ssk149@gmail.com>
2026-09-02 20:00:37 +0530

    Time: 17 ms (64.81%), Space: 44.8 MB (73.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,16 +1,30 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        int left = 0, right = s.Length - 1;
+        int left = 0;
+        int right = s.Length - 1;
 
-        while (left < right) {
-            while (left < right && !Char.IsLetterOrDigit(s[left])) left++;
-            while (left < right && !Char.IsLetterOrDigit(s[right])) right--;
-
-            if (Char.ToLower(s[left]) != Char.ToLower(s[right]))
-                return false;
-
-            left++;
-            right--;
+        while(left < right)
+        {
+            if(!char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+            else if(!char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+            else
+            {
+                if(char.ToLower(s[left]) == char.ToLower(s[right]))
+                {
+                    left++;
+                    right--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         return true;
